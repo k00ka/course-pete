cd94ee0751d357835312abcf137797e55c7ee1e4
petesmithinc@gmail.com <petesmithinc@gmail.com>
2021-04-04 10:47:52 -0400

    Added new fonts and themes to application Added images with icons to the product screen

--- course_app4/lib/screens/products_overview_screen.dart
+++ course_app4/lib/screens/products_overview_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../models/product.dart';
+import '../widgets/product_item.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
@@ -41,15 +42,18 @@ class ProductsOverviewScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('MyShop'),
-      ),
-      body: GridView.builder(
+        appBar: AppBar(
+          title: Text('MyShop'),
+        ),
+        body: GridView.builder(
           padding: const EdgeInsets.all(10.0),
           itemCount: loadedProducts.length,
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2, childAspectRatio: 3/2, crossAxisSpacing: 10, mainAxisSpacing: 10),
-          itemBuilder: (ctx, i) => Container(),
-      )
-    );
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 2,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 10,
+              mainAxisSpacing: 10),
+          itemBuilder: (ctx, i) => ProductItem(loadedProducts[i].id, loadedProducts[i].title, loadedProducts[i].imageUrl,),
+        ));
   }
 }
