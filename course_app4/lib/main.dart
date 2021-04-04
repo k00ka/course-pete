cad658a5dccef04814fe733f26aefc6cd65ea622
petesmithinc@gmail.com <petesmithinc@gmail.com>
2021-04-04 19:42:14 -0400

    Getting the favorites to work with the new state mgmt paradigm, state for favorite managed in widgets

--- course_app4/lib/main.dart
+++ course_app4/lib/main.dart
@@ -10,7 +10,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
+    return ChangeNotifierProvider
+      (
       create: (ctx) => Products(),
       child: MaterialApp(
           title: 'MyShop',
--- course_app4/lib/providers/products.dart
+++ course_app4/lib/providers/products.dart
@@ -37,9 +37,20 @@ class Products with ChangeNotifier {
       'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Cast-Iron-Pan.jpg/1024px-Cast-Iron-Pan.jpg',
     ),
   ];
+//var _showFavoritesOnly = false;
 
   List<Product> get items {
+ //   if(_showFavoritesOnly){
+//      return _items.where((prodItem) => prodItem.isFavorite).toList();
+//    }
     return [..._items];
+  }
+
+  List<Product> get favoriteItems {
+    //   if(_showFavoritesOnly){
+//      return _items.where((prodItem) => prodItem.isFavorite).toList();
+//    }
+    return _items.where((prodItem) => prodItem.isFavorite).toList();
   }
 
   Product findById(String id) {
