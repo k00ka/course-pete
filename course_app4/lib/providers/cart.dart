c3da2375f3ae2be679ad81342ccdf3b3b0387f0e
petesmithinc@gmail.com <petesmithinc@gmail.com>
2021-04-05 20:39:17 -0400

    Added the Cart screen and badge objects, updated the cart listener

--- course_app4/lib/providers/cart.dart
+++ course_app4/lib/providers/cart.dart
@@ -4,7 +4,7 @@ import 'package:flutter/foundation.dart';
 class CartItem {
   final String id;
   final String title;
-  final quantity;
+  final int quantity;
   final double price;
 
   CartItem({
@@ -16,25 +16,51 @@ class CartItem {
 }
 
 class Cart with ChangeNotifier {
-  Map<String, CartItem> _items;
+  Map<String, CartItem> _items = {};
 
   Map<String, CartItem> get items {
     return {..._items};
   }
 
-  void addItem(String productId, double price, String title,) {
+  int get itemCount {
+    return _items.length;
+  }
+
+double get totalAmount {
+    double total = 0.0;
+    _items.forEach((key, cartItem) {
+      total += cartItem.price * cartItem.quantity;
+    });
+    return total;
+}
+
+  void addItem(
+      String productId,
+      double price,
+      String title,
+      ) {
     if (_items.containsKey(productId)) {
-      _items.update(productId, (existingCartItem) =>
-          CartItem(id: existingCartItem.id,
-              title: existingCartItem.title,
-              quantity: existingCartItem.quantity + 1,
-              price: existingCartItem.price),);
+      // change quantity...
+      _items.update(
+        productId,
+            (existingCartItem) => CartItem(
+          id: existingCartItem.id,
+          title: existingCartItem.title,
+          price: existingCartItem.price,
+          quantity: existingCartItem.quantity + 1,
+        ),
+      );
     } else {
-      _items.putIfAbsent(productId, () =>
-          CartItem(id: DateTime.now().toString(),
-              title: title,
-              quantity: 1,
-              price: price))
+      _items.putIfAbsent(
+        productId,
+            () => CartItem(
+          id: DateTime.now().toString(),
+          title: title,
+          price: price,
+          quantity: 1,
+        ),
+      );
     }
+    notifyListeners();
   }
 }
--- course_app4/lib/screens/products_overview_screen.dart
+++ course_app4/lib/screens/products_overview_screen.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_complete_guide/screens/cart_screen.dart';
 import 'package:provider/provider.dart';
 
+import './cart_screen.dart';
 import '../widgets/products_grid.dart';
-import '../providers/products.dart';
+import '../widgets/badge.dart';
+import '../providers/cart.dart';
 
 enum FilterOptions {
   Favorites,
@@ -20,8 +23,10 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('MyShop'), actions: <Widget>[
-        PopupMenuButton(
+      appBar: AppBar(
+        title: Text('MyShop'),
+        actions: <Widget>[
+          PopupMenuButton(
             onSelected: (FilterOptions selectedValue) {
               setState(() {
                 if (selectedValue == FilterOptions.Favorites) {
@@ -31,15 +36,36 @@ class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
                 }
               });
             },
-            icon: Icon(Icons.more_vert),
+            icon: Icon(
+              Icons.more_vert,
+            ),
             itemBuilder: (_) => [
-                  PopupMenuItem(
-                      child: Text('Only Favorites'),
-                      value: FilterOptions.Favorites),
-                  PopupMenuItem(
-                      child: Text('Show All'), value: FilterOptions.All),
-                ]),
-      ]),
+              PopupMenuItem(
+                child: Text('Only Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('Show All'),
+                value: FilterOptions.All,
+              ),
+            ],
+          ),
+          Consumer<Cart>(
+            builder: (_, cart, ch) => Badge(
+              child: ch,
+              value: cart.itemCount.toString(),
+            ),
+            child: IconButton(
+              icon: Icon(
+                Icons.shopping_cart,
+              ),
+              onPressed: () {
+                Navigator.of(context).pushNamed(CartScreen.routeName);
+              },
+            ),
+          ),
+        ],
+      ),
       body: ProductsGrid(_showOnlyFavorites),
     );
   }
