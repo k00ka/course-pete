e0bfe4f575d13baad881a6015655d4bff902c12b
petesmithinc@gmail.com <petesmithinc@gmail.com>
2021-05-15 11:08:27 -0400

    Add the location pacakge to the App so we can get the current geolocation for the add places page
    Note: if you want to run course app 5 on MacOS you will need to properly sandbox it see details in the spec https://pub.dev/packages/location (location services)

---
+++ course_app5/lib/widgets/location_input.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:location/location.dart';
+
+class LocationInput extends StatefulWidget {
+  const LocationInput({Key key}) : super(key: key);
+
+  @override
+  _LocationInputState createState() => _LocationInputState();
+}
+
+class _LocationInputState extends State<LocationInput> {
+  String _previewImgageUrl;
+
+  Future<void> _getCurrentUserLocation() async {
+    Location location = new Location();
+
+    bool _serviceEnabled;
+    PermissionStatus _permissionGranted;
+    LocationData _locationData;
+
+    _serviceEnabled = await location.serviceEnabled();
+    if (!_serviceEnabled) {
+      _serviceEnabled = await location.requestService();
+      if (!_serviceEnabled) {
+        return;
+      }
+    }
+
+    _permissionGranted = await location.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await location.requestPermission();
+      if (_permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    final locData = await location.getLocation();
+    print(locData.latitude);
+    print(locData.longitude);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Container(
+          height: 170,
+          width: double.infinity,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(border: Border.all(width: 1, color: Colors.grey), ),
+          child: _previewImgageUrl == null
+              ? Text(
+                  'No location Chosen',
+                  textAlign: TextAlign.center,
+                )
+              : Image.network(
+                  _previewImgageUrl,
+                  fit: BoxFit.cover,
+                  width: double.infinity,
+                ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton.icon(
+                icon: Icon(Icons.location_on),
+                label: Text('Current Location'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: _getCurrentUserLocation),
+            FlatButton.icon(
+                icon: Icon(Icons.map),
+                label: Text('Select on Map'),
+                textColor: Theme.of(context).primaryColor,
+                onPressed: () {}),
+          ],
+        )
+      ],
+    );
+  }
+}
